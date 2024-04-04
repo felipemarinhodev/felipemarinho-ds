7d89bbb4e35cd818f3e69f82bb5e5bf01513de55
Felipe Marinho <github@felipemarinho.dev>
2024-04-03 18:53:34 -0700

    `theme` add default theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,43 @@
+export default {
+  colors: {
+    primary: '#3D405B',
+    secondary: '#81B29A',
+    tertiary: '#F2CC8F',
+  },
+  border: {
+    radius: '0.4rem',
+  },
+  font: {
+    light: 300,
+    normal: 400,
+    semibold: 500,
+    bold: 600,
+    sizes: {
+      xsmall: '.8rem',
+      small: '1rem',
+      medium: '1.4rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem',
+    },
+    colors: {
+      primary: '#F4F1DE',
+      secondary: '#3D405B',
+      tertiary: '#E07A5F',
+    },
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out',
+  },
+} as const;
